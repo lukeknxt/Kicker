5e7a25a13af08f9bc8ff1ba15678e334e9b258c4
Toheriman <e2h3in@gmail.com>
2015-11-20 10:53:01 +0200

    Neat functions, deepth not working

--- Kicker.js
+++ Kicker.js
@@ -1,6 +1,7 @@
 function count() {
   var radiusIs, angleIs;
   var height = document.getElementById('height').value;
+
   var angle = document.getElementById('angle').value;
   var radius = document.getElementById('radius').value;
   console.log(height, angle, radius);
@@ -9,31 +10,39 @@ function count() {
   var foot = document.getElementById('foot');
   var deepth = document.getElementById('deepth');
 
-  function radiusOrAngle(radius, angle) {
-  if (!radius) {
-    radiusIs = Math.round(height / (2 * (Math.sin(angleRad / 2) * Math.sin(angleRad / 2))) * 100) / 100;
-    document.getElementById('radius').value = radiusIs;
-    console.log(radiusIs);
-  }
-  if (!angle) {
-    angleIs = (2 * Math.asin(Math.sqrt(height / (2 * radius))) / drc).toFixed(2);
-    document.getElementById('angle').value = angleIs;
-    console.log(angleIs);
-  }
-}
+  var drc = 0.0174532925; // degree to radian constant
+  var angleRad = drc * isAngle(angle); // radius angle in radian
+  var angleBrad = ((180 - isAngle(angle)) / 2) * drc; //reversed angle in radian, needed for calc
 
-var drc = 0.0174532925; // degree to radian constant
-var angleRad = angle * drc; // radius angle in radian
-var angleBrad = ((180 - angle) / 2) * drc; //reversed angle in radian, needed for calc
-
+  document.getElementById('radius').value = isRadius(radius);
+  document.getElementById('angle').value = isAngle(angle);
 
   length.innerHTML = (height / Math.sin(angleRad / 2)).toFixed(2);
 
   foot.innerHTML = (height * Math.tan(angleBrad)).toFixed(2);
 
-  radiusOrAngle(radius, angle);
 
-  deepth.innerHTML = (radiusIs - radiusIs * Math.sin((90 - angle / 2) * drc)).toFixed(2);
+  function isAngle(angle) {
+    if (!angle) {
+      return (2 * Math.asin(Math.sqrt(height / (2 * radius))) / drc).toFixed(2);
+    } else {
+      return angle;
+    }
+  }
 
+  function isRadius(radius) {
+    if (!radius) {
+      return Math.round(height / (2 * (Math.sin(angleRad / 2) * Math.sin(angleRad / 2))) * 100) / 100;
+    } else {
+      return radius;
+    }
+  }
+
+  function deepthInnerHTML() {
+    return (isRadius(radius) - isRadius(radius) * Math.sin((90 - isAngle(angle) / 2) * drc)).toFixed(2);
+  }
+
+  deepth.innerHTML = deepthInnerHTML;
+  console.log(deepth);
 
 }
---
+++ test.js
@@ -0,0 +1,8 @@
+function test() {
+  var a = prompt("Enter radius");
+  if (!a) {
+     alert(20);
+  } else {
+    alert(a);
+  }
+}
