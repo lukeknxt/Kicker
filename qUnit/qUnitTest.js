ff918af9c755897e8527a94216c354c9345b69e0
ToherIman <e2h3in@gmail.com>
2017-01-05 18:15:30 +0100

    "radius test"

--- qUnit/qUnitTest.js
+++ qUnit/qUnitTest.js
@@ -1,3 +1,7 @@
 QUnit.test("Angle test", function( assert ) {
   assert.equal(isAngle(60), 60, "Angle is tested");
 });
+
+QUnit.test("Radius test", function( assert ) {
+  assert.equal(isRadius(60), 60, "Radius is tested");
+});
