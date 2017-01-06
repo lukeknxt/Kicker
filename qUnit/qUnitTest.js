b8d8b6a915fa748888477f6f2bf684c236ab8333
ToherIman <e2h3in@gmail.com>
2017-01-06 09:22:17 +0100

    Noradius test

--- qUnit/qUnitTest.js
+++ qUnit/qUnitTest.js
@@ -1,7 +1,8 @@
 QUnit.test("Angle test", function( assert ) {
   assert.equal(isAngle(60), 60, "Angle is tested");
 });
-
+ var angleRad = 1, height = 2;
 QUnit.test("Radius test", function( assert ) {
-  assert.equal(isRadius(60), 60, "Radius is tested");
-});
+  assert.equal(isRadius(60), 60, "Radius given");
+  assert.equal(isRadius(null), 4.35, "Radius calc");
+}); 
